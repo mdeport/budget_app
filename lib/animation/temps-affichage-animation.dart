3a60aceb23ce44e85dec01f49477956b038a3d96
mdeport <mateo.deport@ecole-isitech.fr>
2024-01-21 19:21:06 +0100

    création d'un fichier animation pour que les element dans un page arrive avec une animation et création du fichier de la premiere page d'authentification

---
+++ lib/animation/temps-affichage-animation.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class TempsAnimation extends StatefulWidget {
+  final Widget child;
+  final int delai;
+  const TempsAnimation({required this.child, required this.delai});
+
+  @override
+  State<TempsAnimation> createState() => _TempsAnimationState();
+}
+
+class _TempsAnimationState extends State<TempsAnimation>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _animOffset;
+
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+
+    final curve = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.decelerate,
+    );
+
+    _animOffset = Tween<Offset>(
+      begin: const Offset(0.0, 0.35),
+      end: Offset.zero,
+    ).animate(curve);
+
+    Timer(Duration(milliseconds: widget.delai), () {
+      _controller.forward();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeTransition(
+        opacity: _controller,
+        child: SlideTransition(position: _animOffset, child: widget.child));
+  }
+}
---
+++ lib/pages/page-authentification/page-bienvenue.dart
@@ -0,0 +1,13 @@
+import 'package:application_budget_app/animation/temps-affichage-animation.dart';
+import 'package:flutter/material.dart';
+
+class bienvenuePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('bienvenue'),
+      ),
+    );
+  }
+}
