ed316d132216d8cc5e4c40f8a7ac4cec11251208
mdeport <mateo.deport@ecole-isitech.fr>
2024-02-11 19:24:26 +0100

    commencer a faire la page de presentation pour créer un compte il resteras a mettre le champs texte adresse mail et mdp

--- lib/pages/page-authentification/page-creer-compte.dart
+++ lib/pages/page-authentification/page-creer-compte.dart
@@ -1,11 +1,93 @@
+import 'package:application_budget_app/main.dart';
 import 'package:flutter/material.dart';
+import 'package:application_budget_app/animation/temps-affichage-animation.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class page_creation_compte extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Créer un compte'),
+        backgroundColor: Colors.white.withOpacity(0),
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+            size: 30,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 135, horizontal: 40),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TempsAnimation(
+                    delai: 1000,
+                    child: Text(
+                      'Créer un compte',
+                      style: GoogleFonts.poppins(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 22),
+                  TempsAnimation(
+                    delai: 2500,
+                    child: Text(
+                      "Il est recommandé de connecter votre adresse e-mail pour que nous puissions mieux protéger vos informations.",
+                      style: GoogleFonts.poppins(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 35),
+            // LoginForm(),
+            SizedBox(height: 125),
+            TempsAnimation(
+              delai: 5500,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blue[900],
+                  shape: StadiumBorder(),
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 125,
+                    vertical: 13,
+                  ),
+                ),
+                child: Text(
+                  "CONFIRMER",
+                  style: GoogleFonts.poppins(
+                    fontSize: 15,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => MyApp(),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
