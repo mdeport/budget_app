dbabc489f1f20df363d93e370ac71ca0c8cfa0c1
mdeport <mateo.deport@ecole-isitech.fr>
2024-02-13 15:57:33 +0100

    les deux pages ont été créer reste a faire le lien avec la base de données et mettre des element de validation pour enregistre le mdp et mail comme @ et mini 8 caractère dans mdp

--- lib/pages/page-authentification/page-connexion.dart
+++ lib/pages/page-authentification/page-connexion.dart
@@ -1,11 +1,147 @@
+import 'package:application_budget_app/main.dart';
 import 'package:flutter/material.dart';
+import 'package:application_budget_app/animation/temps-affichage-animation.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class page_connexion_compte extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('connexion'),
+        elevation: 0,
+        backgroundColor: Colors.white.withOpacity(0),
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+            size: 30,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 70, horizontal: 40),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Connexion',
+                    style: GoogleFonts.poppins(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  SizedBox(height: 22),
+                  Text(
+                    "Pour accéder à votre compte en toute sécurité, veuillez vous connecter avec votre adresse e-mail.",
+                    style: GoogleFonts.poppins(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            LoginForm(),
+            SizedBox(height: 45),
+            TempsAnimation(
+              delai: 500,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blue[900],
+                  shape: StadiumBorder(),
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 125,
+                    vertical: 13,
+                  ),
+                ),
+                child: Text(
+                  "CONNEXION",
+                  style: GoogleFonts.poppins(
+                    fontSize: 15,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => MyApp(),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LoginForm extends StatefulWidget {
+  @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  var _obsuretext1 = true;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
+        children: [
+          TextFormField(
+            decoration: InputDecoration(
+              labelText: 'Adresse e-mail',
+              labelStyle: GoogleFonts.poppins(
+                fontSize: 15,
+                fontWeight: FontWeight.w500,
+                color: Colors.grey,
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          TextFormField(
+            obscureText: _obsuretext1,
+            decoration: InputDecoration(
+              labelText: 'Mot de passe',
+              labelStyle: GoogleFonts.poppins(
+                fontSize: 15,
+                fontWeight: FontWeight.w500,
+                color: Colors.grey,
+              ),
+              /*suffixIcon: IconButton(
+                icon: const Icon(
+                  Icons.visibility,
+                  color: Colors.black,
+                ),
+                onPressed: () {
+                  setState(
+                    () {
+                      _obsuretext1 = !_obsuretext1;
+                    },
+                  );
+                },
+              ),*/
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/pages/page-authentification/page-creer-compte.dart
+++ lib/pages/page-authentification/page-creer-compte.dart
@@ -8,6 +8,7 @@ class page_creation_compte extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 0,
         backgroundColor: Colors.white.withOpacity(0),
         leading: IconButton(
           icon: const Icon(
@@ -24,46 +25,39 @@ class page_creation_compte extends StatelessWidget {
         child: Column(
           children: [
             Container(
-              margin: const EdgeInsets.symmetric(vertical: 135, horizontal: 40),
+              margin: const EdgeInsets.symmetric(vertical: 70, horizontal: 40),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  TempsAnimation(
-                    delai: 1000,
-                    child: Text(
-                      'Créer un compte',
-                      style: GoogleFonts.poppins(
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.blue,
-                      ),
+                  Text(
+                    'Créer un compte',
+                    style: GoogleFonts.poppins(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue,
                     ),
                   ),
-                  SizedBox(height: 22),
-                  TempsAnimation(
-                    delai: 2500,
-                    child: Text(
-                      "Il est recommandé de connecter votre adresse e-mail pour que nous puissions mieux protéger vos informations.",
-                      style: GoogleFonts.poppins(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500,
-                        color: Colors.grey,
-                      ),
+                  const SizedBox(height: 22),
+                  Text(
+                    "Il est recommandé de connecter votre adresse e-mail pour que nous puissions mieux protéger vos informations.",
+                    style: GoogleFonts.poppins(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.grey,
                     ),
                   ),
                 ],
               ),
             ),
-            SizedBox(height: 35),
-            // LoginForm(),
-            SizedBox(height: 125),
+            LoginForm(),
+            const SizedBox(height: 45),
             TempsAnimation(
-              delai: 5500,
+              delai: 500,
               child: ElevatedButton(
                 style: ElevatedButton.styleFrom(
                   primary: Colors.blue[900],
-                  shape: StadiumBorder(),
-                  padding: EdgeInsets.symmetric(
+                  shape: const StadiumBorder(),
+                  padding: const EdgeInsets.symmetric(
                     horizontal: 125,
                     vertical: 13,
                   ),
@@ -77,17 +71,110 @@ class page_creation_compte extends StatelessWidget {
                   ),
                 ),
                 onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => MyApp(),
-                    ),
-                  );
+                  print('Création de compte');
                 },
               ),
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class LoginForm extends StatefulWidget {
+  @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  var _obsuretext1 = true;
+  var _obsuretext2 = true;
+  var _email;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final RegExp emailRegex = RegExp(r"[a-z0-9\._-]+@[a-z0-9\._-]+\.[a-z]+");
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 30),
+      key: _formKey,
+      child: Column(
+        children: [
+          TextFormField(
+            onChanged: (value) => setState(() => _email = value),
+            validator: (value) => emailRegex.hasMatch(value!)
+                ? null
+                : 'Entrez une adresse e-mail valide',
+            decoration: InputDecoration(
+              labelText: 'Adresse e-mail',
+              labelStyle: GoogleFonts.poppins(
+                fontSize: 15,
+                fontWeight: FontWeight.w500,
+                color: Colors.grey,
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          TextFormField(
+            obscureText: _obsuretext1,
+            decoration: InputDecoration(
+              labelText: 'Mot de passe',
+              labelStyle: GoogleFonts.poppins(
+                fontSize: 15,
+                fontWeight: FontWeight.w500,
+                color: Colors.grey,
+              ),
+              /*suffixIcon: IconButton(
+                icon: const Icon(
+                  Icons.visibility,
+                  color: Colors.black,
+                ),
+                onPressed: () {
+                  setState(
+                    () {
+                      _obsuretext1 = !_obsuretext1;
+                    },
+                  );
+                },
+              ),*/
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          TextFormField(
+            obscureText: _obsuretext2,
+            decoration: InputDecoration(
+              labelText: 'Confirmer le mot de passe',
+              labelStyle: GoogleFonts.poppins(
+                fontSize: 15,
+                fontWeight: FontWeight.w500,
+                color: Colors.grey,
+              ),
+              /*suffixIcon: IconButton(
+                icon: const Icon(
+                  Icons.visibility,
+                  color: Colors.black,
+                ),
+                onPressed: () {
+                  setState(
+                    () {
+                      _obsuretext2 = !_obsuretext2;
+                    },
+                  );
+                },
+              ),*/
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
