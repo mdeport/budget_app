c7c3ef8fe41ebe254915a8f2bdc09c5b6be93ba4
mdeport <mateo.deport@ecole-isitech.fr>
2024-02-19 21:01:24 +0100

    commit de la branch

--- README.md
+++
@@ -1,3 +0,0 @@
-# application_budget_app
-
-Nouveau projet de gestion de budget
--- android/app/src/main/kotlin/com/example/application_budget_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.application_budget_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
