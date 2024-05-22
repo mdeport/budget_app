19ffe85276060b0e3488a00dd7bcf63684734b95
mdeport <mateo.deport@ecole-isitech.fr>
2024-05-22 23:34:43 +0200

    ajout des données et de premier partie de la page de paramètre

--- lib/pages/page-contenue-app/page-parametre/page-parametre-principal.dart
+++ lib/pages/page-contenue-app/page-parametre/page-parametre-principal.dart
@@ -1,8 +1,174 @@
 import 'package:flutter/material.dart';
 import 'package:application_budget_app/pages/page-authentification/page-social.dart';
 import 'package:application_budget_app/pages/services/UserService.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:settings_ui/settings_ui.dart';
 
 class Page_parametre_principal extends StatefulWidget {
+  const Page_parametre_principal({super.key});
+
+  @override
+  State<Page_parametre_principal> createState() =>
+      _Page_parametre_principalState();
+}
+
+class _Page_parametre_principalState extends State<Page_parametre_principal> {
+  final UserService _userService = UserService();
+  bool _isDark = false;
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: _isDark ? ThemeData.dark() : ThemeData.light(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Paramètres',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 30.0,
+            ),
+          ),
+          centerTitle: true,
+          automaticallyImplyLeading: false,
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+              color: Color(0xFF2196F3), // Bleu pastel
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              ),
+            ),
+          ),
+          backgroundColor: const Color(0xFF2196F3),
+        ),
+        body: Center(
+          child: Container(
+            constraints: const BoxConstraints(maxWidth: 400),
+            child: ListView(
+              children: [
+                _SingleSection(
+                  title: "General",
+                  children: [
+                    _CustomListTile(
+                        title: "Dark Mode",
+                        icon: Icons.dark_mode_outlined,
+                        trailing: Switch(
+                            value: _isDark,
+                            onChanged: (value) {
+                              setState(() {
+                                _isDark = value;
+                              });
+                            })),
+                    const _CustomListTile(
+                        title: "Notifications",
+                        icon: Icons.notifications_none_rounded),
+                    const _CustomListTile(
+                        title: "Security Status",
+                        icon: CupertinoIcons.lock_shield),
+                  ],
+                ),
+                const Divider(),
+                const _SingleSection(
+                  title: "Organization",
+                  children: [
+                    _CustomListTile(
+                        title: "Profile", icon: Icons.person_outline_rounded),
+                    _CustomListTile(
+                        title: "Messaging", icon: Icons.message_outlined),
+                    _CustomListTile(
+                        title: "Calling", icon: Icons.phone_outlined),
+                    _CustomListTile(
+                        title: "People", icon: Icons.contacts_outlined),
+                    _CustomListTile(
+                        title: "Calendar", icon: Icons.calendar_today_rounded)
+                  ],
+                ),
+                const Divider(),
+                const _SingleSection(
+                  children: [
+                    _CustomListTile(
+                        title: "Help & Feedback",
+                        icon: Icons.help_outline_rounded),
+                    _CustomListTile(
+                        title: "About", icon: Icons.info_outline_rounded),
+                    _CustomListTile(
+                        title: "Sign out", icon: Icons.exit_to_app_rounded),
+                  ],
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    await _userService.signOut();
+
+                    Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const PageSocial(),
+                        ),
+                        (route) => false);
+                  },
+                  child: const Text('Se déconnecter'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  final String title;
+  final IconData icon;
+  final Widget? trailing;
+  const _CustomListTile(
+      {Key? key, required this.title, required this.icon, this.trailing})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(title),
+      leading: Icon(icon),
+      trailing: trailing,
+      onTap: () {},
+    );
+  }
+}
+
+class _SingleSection extends StatelessWidget {
+  final String? title;
+  final List<Widget> children;
+  const _SingleSection({
+    Key? key,
+    this.title,
+    required this.children,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (title != null)
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              title!,
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+          ),
+        Column(
+          children: children,
+        ),
+      ],
+    );
+  }
+}
+
+/*class Page_parametre_principal extends StatefulWidget {
   const Page_parametre_principal({super.key});
 
   @override
@@ -16,6 +182,22 @@ class _Page_parametre_principalState extends State<Page_parametre_principal> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Paramètres',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 30.0,
+          ),
+        ),
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(color: Colors.blue),
+        ),
+        backgroundColor: Colors.blue,
+      ),
       body: Center(
         child: ElevatedButton(
           onPressed: () async {
@@ -33,4 +215,4 @@ class _Page_parametre_principalState extends State<Page_parametre_principal> {
       ),
     );
   }
-}
+}*/
